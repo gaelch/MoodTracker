4a6a826382b00946d3403439f71a5eaec4c66453
gaelch <zz-gael@hotmail.fr>
2018-05-10 12:54:08 +0200

    6th commit. Swipe work

--- app/src/main/java/com/cheyrouse/gael/moodtracker/controller/MainActivity.java
+++ app/src/main/java/com/cheyrouse/gael/moodtracker/controller/MainActivity.java
@@ -4,18 +4,22 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.GestureDetector.OnGestureListener;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import com.cheyrouse.gael.moodtracker.R;
 import com.cheyrouse.gael.moodtracker.model.Moods;
+import com.cheyrouse.gael.moodtracker.model.Prefs;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity implements OnGestureListener{
 
@@ -25,18 +29,33 @@ public class MainActivity extends Activity implements OnGestureListener{
     private ArrayList<Moods> moodsLst;
     private int counter;
     private GestureDetector mDetector;
+    private RelativeLayout layout;
+    private View mHistoryButton;
+    private View mNoteAddButton;
+    public static final String PREF_KEY_MOODS = "PREF_KEY_MOODS";
+    private SharedPreferences mPreferences;
+    private List<Moods> Moodstore;
 
-
+    public void updateDisplay() {
+        if( (counter >= 0) && (counter < moodsLst.size()) )
+        {
+            mSmileyMood.setImageDrawable(this.getResources().getDrawable(moodsLst.get(counter).getmSmiley()));
+            mSmileyMood.setBackground(this.getResources().getDrawable(moodsLst.get(counter).getmBackground()));
+            layout.setBackground(this.getResources().getDrawable(moodsLst.get(counter).getmBackground()));
+            mHistoryButton.setBackground(this.getResources().getDrawable(moodsLst.get(counter).getmBackground()));
+            mNoteAddButton.setBackground(this.getResources().getDrawable(moodsLst.get(counter).getmBackground()));
+        }
+    }
     @SuppressLint("ClickableViewAccessibility")
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSmileyMood = findViewById(R.id.activity_main_mood_image);
-        ImageButton mHistoryButton = findViewById(R.id.activity_main_history_button);
-        ImageButton mNoteAddButton = findViewById(R.id.activity_main_note_add_button);
+        mPreferences = getPreferences(MODE_PRIVATE);
 
+        mSmileyMood = findViewById(R.id.activity_main_mood_image);
+        mHistoryButton = findViewById(R.id.activity_main_history_button);
+        mNoteAddButton = findViewById(R.id.activity_main_note_add_button);
 
         mNoteAddButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,16 +74,12 @@ public class MainActivity extends Activity implements OnGestureListener{
                 alertDialogBuilder
                         .setCancelable(true)
                         .setPositiveButton("VALIDER", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                            }
-                        })
+                            public void onClick(DialogInterface dialog, int id) { }})
                         .setNegativeButton("ANNULER", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
                                 // if this button is clicked, just close
                                 // the dialog box and do nothing
-                                dialog.cancel();
-                            }
-                        });
+                                dialog.cancel(); }});
                 // create alert dialog
                 AlertDialog alertDialog = alertDialogBuilder.create();
                 // show it
@@ -78,10 +93,8 @@ public class MainActivity extends Activity implements OnGestureListener{
                 startActivity(historyActivityIntent);
             }
         });
-
-
-        Moods moods1 = new Moods(R.drawable.smiley_happy, R.color.light_sage, 0);
-        Moods moods2 = new Moods(R.drawable.smiley_super_happy, R.color.banana_yellow, 1);
+        Moods moods1 = new Moods(R.drawable.smiley_super_happy, R.color.banana_yellow, 0);
+        Moods moods2 = new Moods(R.drawable.smiley_happy, R.color.light_sage, 1);
         Moods moods3 = new Moods(R.drawable.smiley_normal, R.color.cornflower_blue_65, 2);
         Moods moods4 = new Moods(R.drawable.smiley_disappointed, R.color.warm_grey, 3);
         Moods moods5 = new Moods(R.drawable.smiley_sad, R.color.faded_red, 4);
@@ -93,80 +106,88 @@ public class MainActivity extends Activity implements OnGestureListener{
         moodsLst.add(moods4);
         moodsLst.add(moods5);
 
+        counter = 1;
+
+        /*mPreferences.edit().putInt(PREF_KEY_MOODS, counter).apply();
+        Prefs prefs = Prefs.get(this);
+        ArrayList<Moods> moodstore = prefs.getMoodstore();
+        Moodstore.add(moods2);
+        prefs.storeMoodstore(moodstore);*/
+
         final GestureDetector mDetector = new GestureDetector(MainActivity.this);
 
-        mSmileyMood.setOnTouchListener(new View.OnTouchListener() {
+        layout = findViewById(R.id.activity_main);
+        layout.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-
                 return mDetector.onTouchEvent(event);
-
             }
         });
-
     }
-
     @Override
     protected void onStart() {
         super.onStart();
-
         System.out.println("MainActivity::onStart()");
     }
-
     @Override
     protected void onResume() {
         super.onResume();
-
         System.out.println("MainActivity::onResume()");
     }
-
     @Override
     protected void onPause() {
         super.onPause();
-
         System.out.println("MainActivity::onPause()");
     }
-
     @Override
     protected void onStop() {
         super.onStop();
-
         System.out.println("MainActivity::onStop()");
     }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
         System.out.println("MainActivity::onDestroy()");
     }
-
     @Override
     public boolean onDown(MotionEvent e) {
         return true;
     }
-
     @Override
     public void onShowPress(MotionEvent e) {
 
     }
-
     @Override
     public boolean onSingleTapUp(MotionEvent e) {
         return false;
     }
-
     @Override
     public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
         return false;
     }
-
     @Override
     public void onLongPress(MotionEvent e) {
     }
 
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        return false;
+                if (e1.getY() - e2.getY() > 30)
+                {
+                    if (counter > 0) {
+                        counter--;
+                    }else{
+                        counter = 0;
+                    }
+                }
+                if (e2.getY() - e1.getY() > 30)
+                {
+                    if (counter < moodsLst.size() - 1){
+                        counter++;
+                    }else{
+                        counter = moodsLst.size() - 1;
+                    }
+                }
+                updateDisplay();
+                return true;
     }
 }
--- app/src/main/java/com/cheyrouse/gael/moodtracker/model/Prefs.java
+++ app/src/main/java/com/cheyrouse/gael/moodtracker/model/Prefs.java
@@ -1,6 +1,77 @@
 package com.cheyrouse.gael.moodtracker.model;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
 public class Prefs {
+   /* private static Prefs instance;
+    private static String PREFS_MOODSTORE = "Moodstore";
+    private static String Moods = "Moods";
+    public static ArrayList<com.cheyrouse.gael.moodtracker.model.Moods> get;
+
+    private Context context;
+
+    public SharedPreferences getPrefs() {
+        return prefs;
+    }
+
+    public void setPrefs(SharedPreferences prefs) {
+        this.prefs = prefs;
+    }
+
+    private SharedPreferences prefs;
+    private Moods moods;
 
 
+    private Prefs(Context context) {
+        this.context = context;
+
+        prefs = context.getSharedPreferences(PREFS_MOODSTORE, Activity.MODE_PRIVATE);
+
+    }
+
+    public static Prefs get(Context context) {
+        if (instance == null)
+            instance = new Prefs(context);
+        return instance;
+    }
+
+    public void storeMoodstore(ArrayList<Moods> moodstore) {
+        //start writing (open the file)
+        SharedPreferences.Editor editor = prefs.edit();
+        //put the data
+        Gson gson = new Gson();
+        String json = gson.toJson(moodstore);
+        editor.putString(Moods, json);
+        //close the file
+        editor.apply();
+    }
+
+    public ArrayList<Moods> getMoodstore() {
+        Gson gson = new Gson();
+        String json = prefs.getString(Moods, "");
+
+        ArrayList<Moods> moodstore;
+
+        if (json.length() < 1) {
+            moodstore = new ArrayList<>();
+        } else {
+            Type type = new TypeToken<ArrayList<Moods>>() {
+            }.getType();
+            moodstore = gson.fromJson(json, type);
+        }
+
+        //return the value that was stored under the key
+        //"NAME". If there was no value stored under this key, return null.
+        return moodstore;
+    }*/
 }
+
+
